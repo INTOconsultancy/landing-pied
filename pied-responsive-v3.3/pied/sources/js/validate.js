b4f77380e37bd331990911b771b0e3312b0bd0ac
ErikAdrianBangalter11 <Eduran@piedaterre.world>
2018-04-10 17:49:49 -0500

    Cleaning

--- pied-responsive-v3.3/pied/sources/js/validate.js
+++ pied-responsive-v3.3/pied/sources/js/validate.js
@@ -1,49 +1,43 @@
 $(document).ready(function () {
-    var ercp=/(^([0-9]{5,5})|^)$/;
+    var ercp = /(^([0-9]{5,5})|^)$/;
     var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
     $(".botton").click(function () {
-      $(".error").remove();
+        $(".error").remove();
         if ($(".f_name").val() == "") {
             $(".f_name")
                 .focus()
                 .after("<div class='error'> <span>Required field</span></div>");
             return false;
-        }else if ($(".l_name").val() == "") {
+        } else if ($(".l_name").val() == "") {
             $(".l_name")
                 .focus()
                 .after("<span class='error'>Required field</span>");
             return false;
-        }
-        else if ($(".email").val() == "" || !emailreg.test($(".email").val())) {
+        } else if ($(".email").val() == "" || !emailreg.test($(".email").val())) {
             $(".email")
                 .focus()
                 .after("<span class='error'>Required valid email</span>");
             return false;
 
-        }
-        else if ($(".zc").val() == ""|| !ercp.test($(".zc").val())) {
+        } else if ($(".zc").val() == "" || !ercp.test($(".zc").val())) {
             $(".zc")
                 .focus()
                 .after("<span class='error'>Required valid field</span>");
             return false;
-        }
-        else if ($(".p_number").val() == "") {
+        } else if ($(".p_number").val() == "") {
             $(".p_number")
                 .focus()
                 .after("<span class='error'>Required field </span>");
             return false;
-        }
-        else if ($(".country").val() == "") {
+        } else if ($(".country").val() == "") {
             $(".country")
                 .focus()
                 .after("<span class='error'>Required field </span>");
             return false;
-        }
-
-        else if ($(".f_name, .l_name, .email, .zc, .p_number, .country").val() != "") {
-          $(".country")
-              .focus()
-              .after("<div class='succes'> <span>Thanks for being part of us </span>");
+        } else if ($(".f_name, .l_name, .email, .zc, .p_number, .country").val() != "") {
+            $(".country")
+                .focus()
+                .after("<div class='succes'> <span>Thanks for being part of us </span>");
         }
     });
 
@@ -61,12 +55,14 @@ $(document).ready(function () {
             return false;
         }
     });
-    
+
     //scroll//
-    $(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+    $(function () {
+        $('a[href*=#]').on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 500, 'linear');
+        });
     });
 });
